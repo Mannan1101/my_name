a4dc4742d48176047572434aa4dc2c9af5e39ce3
Abdul Mannan <161303446+Mannan1101@users.noreply.github.com>
2024-03-16 02:33:21 +0500

    Add files via upload
    This is my 2nd github project

---
+++ main.js
@@ -0,0 +1,5 @@
+var myname = ("abdul mannan");
+var myage = 32;
+var isjobless = false;
+console.log(myname, myage, isjobless);
+console.log(myname.toLocaleUpperCase());
---
+++ main.ts
@@ -0,0 +1,6 @@
+let myname:string = ("abdul mannan");
+let myage:number = 32;
+let isjobless:boolean = false;
+
+console.log(myname,myage,isjobless);
+console.log(myname.toLocaleUpperCase());
